0d235bcdacda773507d28a8790509cfc3ecbee4a
Puneet Mahajan <puneetonly12@gmail.com>
2025-08-26 17:06:37 +0200

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Puneet Mahajan on 26/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
